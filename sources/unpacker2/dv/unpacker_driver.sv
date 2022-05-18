299d09669295117316e23a613432d4b41249630c
Javier <jeacostav7@gmail.com>
2022-05-17 20:35:20 -0600

    ´driver´

--- sources/unpacker2/dv/unpacker_driver.sv
+++ sources/unpacker2/dv/unpacker_driver.sv
@@ -20,7 +20,7 @@ class unpacker_driver #(max_pkt_size=160) extends uvm_driver#(unpacker_transacti
 
    virtual task drive();
       unpacker_transaction tx;
-      integer packet_counter = 0, state = 0, data_size = 0, remainning_bytes = 0, temp_data = 160'b0;
+      integer packet_counter = 0, send = 0, data_size = 0, remainning_bytes = 0, temp_data = 160'b0;
       `uvm_info(get_full_name(), "driver: start",UVM_LOW)
 
       vif.sig_reset_L = 1'b0;
@@ -30,58 +30,55 @@ class unpacker_driver #(max_pkt_size=160) extends uvm_driver#(unpacker_transacti
       vif.sig_vbc = 8'b0;
       vif.sig_data = 1279'b0;
       forever begin
-         if(remainning_bytes == 0)
+         vif.sig_reset_L = 1'b1;
+
+         if(send == 0)
          begin
             seq_item_port.get_next_item(tx);
             `uvm_info("unpacker_driver", tx.sprint(), UVM_LOW);
             data_size = tx.pkt.size;
             if(data_size != 0)
             begin
-               state = 1;
+               remainning_bytes = data_size;
+               vif.sig_sop = 1'b1;
+               send = 1;
             end
          end
 
+         //assign vif.sig_eop = (remainning_bytes < 160 && send == 1);   
+
          @(posedge vif.sig_clock)
             begin
-               case(state)
-                  1: 
-                  begin
-                     remainning_bytes = data_size;
-                     state = 2;
-                  end
-                  2:
-                  begin
-                     vif.sig_reset_L = 1'b1;
+               vif.sig_eop = (remainning_bytes < 160 && send == 1);
+
+               if (vif.sig_ready  == 1'b1 && send == 1)
+                  begin  
                      if(remainning_bytes == data_size)
                      begin
-                        vif.sig_sop = 1'b1;
                         vif.sig_val = 1'b1;
                      end
+                     temp_data = tx.pkt.data[max_pkt_size*packet_counter*8 +: max_pkt_size*8];
                      if(remainning_bytes >= 160)
                      begin
-                        vif.sig_data = tx.pkt.data[max_pkt_size*packet_counter*8 +: max_pkt_size*8];
+                        vif.sig_data = temp_data;
                         vif.sig_vbc = 8'd160;
                         remainning_bytes = remainning_bytes - 160;
                         packet_counter++; 
-                     end
-                     if(remainning_bytes < 160)
-                     begin
-                        temp_data = tx.pkt.data >> (max_pkt_size-remainning_bytes)*8;
-                        vif.sig_data = temp_data;
+                     end else begin
+                        vif.sig_data = temp_data >> (max_pkt_size-remainning_bytes)*8;;
                         vif.sig_vbc = remainning_bytes;
-                        state = 3;
+                        seq_item_port.item_done();
+                        packet_counter = 0;
+                        remainning_bytes = 0;
+                        send = 0;
                      end
                   end
-                  3:
-                  begin
-                     vif.sig_eop = 1;
-                     vif.sig_val = 1'b0;
-                     vif.sig_vbc = 0;
-                     packet_counter = 0;
-                     remainning_bytes = 0;
-                     seq_item_port.item_done();   
-                  end
-               endcase
+
+               if (remainning_bytes < data_size)
+               begin
+                  vif.sig_sop = 1'b0;
+               end
+
               // `uvm_info("driver tx", tx.sprint(), UVM_LOW);
            end
       end
