09b30443d305c8d6d95ae0603d8ad1d911b49cd7
Esteban Zamora <esteban.zamora.al@gmail.com>
2022-05-18 18:45:02 -0600

    Debug unpacker driver back-to-back
    Signed-off-by: Esteban Zamora <esteban.zamora.al@gmail.com>

--- sources/unpacker2/dv/unpacker_test.sv
+++ sources/unpacker2/dv/unpacker_test.sv
@@ -34,8 +34,9 @@ class unpacker_test extends uvm_test;
 
       phase.raise_objection(.obj(this));
 
-      send_pkt_seq_size(32, 10);
-      // send_pkt_seq_size(160, 1);
+      // send_pkt_seq_size(32, 5);
+      // send_pkt_seq_size(162, 5);
+      send_pkt_seq_size(512, 2);
 
       // `uvm_info("test", "Testing small packet sizes", UVM_LOW);
       // send_pkt_seq_size_range(1, 159, 10);
