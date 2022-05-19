bf79085e234e20059d25c6e82bd6ffc47f1b44ec
Javier <jeacostav7@gmail.com>
2022-05-18 19:46:03 -0600

    ´unpacker_javier_deleted´

--- sources/unpacker_javier/dv/simpleadder_config.sv
+++
@@ -1,8 +0,0 @@
-import uvm_pkg::*;
-class simpleadder_configuration extends uvm_object;
-	`uvm_object_utils(simpleadder_configuration)
-
-	function new(string name = "");
-		super.new(name);
-	endfunction: new
-endclass: simpleadder_configuration
--- sources/unpacker_javier/dv/simpleadder_if.sv
+++
@@ -1,9 +0,0 @@
-interface simpleadder_if;
-	logic		sig_clock;
-	logic		sig_ina;
-	logic		sig_inb;
-	logic		sig_en_i;
-
-	logic		sig_out;
-	logic		sig_en_o;
-endinterface: simpleadder_if
